76180d50a23288e15f8d1b20678c84142ebe7c06
Sergey <relzet69@gmail.com>
2019-03-19 00:20:29 +0200

    Updates

---
+++ precache-manifest.4530487f1ec948484c70bb557ad05944.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c0c4e8d64904b02a0909bee104d179c",
+    "url": "/react-pro-camp/static/media/poster_none.4c0c4e8d.png"
+  },
+  {
+    "revision": "dcb304b27ce2cc944081",
+    "url": "/react-pro-camp/static/js/runtime~main.8f90ed19.js"
+  },
+  {
+    "revision": "d87f6e17186a331d0ebc",
+    "url": "/react-pro-camp/static/js/main.8777045e.chunk.js"
+  },
+  {
+    "revision": "3b40eaa10ece843b63dd",
+    "url": "/react-pro-camp/static/js/2.34d1b48b.chunk.js"
+  },
+  {
+    "revision": "8b7ab37fcbdf2fbf0a2a5342fc600fb9",
+    "url": "/react-pro-camp/index.html"
+  }
+];
